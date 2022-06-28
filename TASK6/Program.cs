150bd386d3f2b83bcb166db21501556c18ea249b
TIM27 <sprinchak27@mail.ru>
2022-06-28 23:45:19 +0300

    Решение задания 6

--- TASK6/Program.cs
+++ TASK6/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдает является лм оно четным (делится на 2 без остатка).
+namespace HelloWorld
+{
+    class Program 
+    {
+        static void Main(string[] args)
+
+        {
+            int number;
+    
+            Console.WriteLine("Введите целое число: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            
+
+            if (number % 2 ==0)
+            {
+                Console.WriteLine("Число {number} является четным и делится на 2 без остатка");
+            }
+            else
+            {
+                Console.WriteLine("Число {number} не является четным и не делится на 2 без остатка");
+            }
+
+
+        }
+    }
+
+
+}
