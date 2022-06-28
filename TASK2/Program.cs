f01a9e33fb69e46d63ec0f7ea48422ca0400211e
TIM27 <sprinchak27@mail.ru>
2022-06-28 22:06:17 +0300

    Решение задания 2

--- TASK2/Program.cs
+++ TASK2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдает какое число больше а какое меньше.
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int A, B;
+            Console.WriteLine ("Введите целое число A: ");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine ("Введите  целое число B: ");
+            B = Convert.ToInt32(Console.ReadLine());
+            if ( A > B )
+            {
+                Console.WriteLine($"Число {A} является наибольшим числом, а число {B} наименьшим");
+            }
+            else
+            {
+                Console.WriteLine("Число {B} является наибольшим числом, а число {A} наименьшим");
+            }
+            
+
+        }
+    }
+       
+}
