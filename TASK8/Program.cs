04e1ae57f86ec7d9ecf383b532c53cc6cfa4109a
TIM27 <sprinchak27@mail.ru>
2022-06-29 19:48:17 +0300

    Решение задания 8

--- TASK8/Program.cs
+++ TASK8/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные от 1 до N.
+namespace HelloWorld
+{
+    class Program 
+    {
+        static void Main(string[] args)
+
+        {    
+            
+        int m = 1;
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = m; i <= n; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
+            
+        }
+    }
+
+
+}
+
